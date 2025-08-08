24b61b906c9a2cd2063c57d1a2bdf84491893690
Akshansh <akshansh.akshansh@gojek.com>
2025-08-08 10:31:44 +0530

    Shell script clean up

--- shine_light.sh
+++ shine_light.sh
@@ -4,14 +4,6 @@ set -e
 
 MODULE_NAME=""
 
-# Name of the Kotlin file
-KOTLIN_FILE="LightSource"
-KOTLIN_SRC="./src/main/kotlin/com/akshansh/app/${KOTLIN_FILE}.kt"
-KOTLIN_OUT="./src/main/kotlin/com/akshansh/app/${KOTLIN_FILE}.jar"
-KOTLIN_HOME=$(dirname $(which kotlinc))/../lib
-KOTLIN_LIBS="$KOTLIN_HOME/kotlin-reflect.jar"
-ANNOTATIONS_JAR="./core/build/libs/core-1.0-SNAPSHOT.jar"
-
 # Parse flags
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -36,18 +28,6 @@ echo "Running Gradle task: core"
 ./gradlew shadowJar
 
 JAR_PATH="./build/libs/reflect-gen.jar"
-
-## Compile
-#echo "Shining light..."
-#kotlinc "$KOTLIN_SRC" -classpath "$ANNOTATIONS_JAR:$KOTLIN_LIBS" -include-runtime -d "$KOTLIN_OUT"
-#
-## Run
-#if [ $? -eq 0 ]; then
-#  echo "Running $KOTLIN_OUT..."
-#  java -jar "$KOTLIN_OUT"
-#else
-#  echo "Compilation failed."
-#fi
 
 if [[ ! -f "$JAR_PATH" ]]; then
   echo "Fat JAR not found at $JAR_PATH"
